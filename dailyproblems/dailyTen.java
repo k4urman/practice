13238ffec47e0c2a3d7488efe8f716bb6bd0cbc4
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-01-04 19:06:41 -0500

    UpdateLast dailyTen.java

--- dailyproblems/dailyTen.java
+++ dailyproblems/dailyTen.java
@@ -7,10 +7,9 @@ You have to rotate the image in-place, which means you have to modify the input 
     
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix.length; j++) {
                 int temp = matrix[i][j];
 
                 matrix[i][j] = matrix[j][i];
@@ -18,12 +17,12 @@ class Solution {
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n / 2; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
                 int temp = matrix[i][j];
 
-                matrix[i][j] = matrix[i][n - j - 1];
-                matrix[i][n - j - 1] = temp;
+                matrix[i][j] = matrix[i][matrix.length - j - 1];
+                matrix[i][matrix.length - j - 1] = temp;
             }
 
         }
