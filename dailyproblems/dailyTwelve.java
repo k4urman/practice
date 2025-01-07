7310a3585fcdaaa2fa240d5bd9af6bffcfa0b2b3
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-01-07 14:50:16 -0500

    Update6 dailyTwelve.java

--- dailyproblems/dailyTwelve.java
+++ dailyproblems/dailyTwelve.java
@@ -8,6 +8,12 @@ Output: [[1,0,1],[0,0,0],[1,0,1]]
 
 Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+Constraints:
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1
 */
 
 class Solution {
