3d0184d2cd4d731c95a73600ed43c67c9a356891
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-01-07 14:49:13 -0500

    Update3 dailyTwelve.java

--- dailyproblems/dailyTwelve.java
+++ dailyproblems/dailyTwelve.java
@@ -2,22 +2,23 @@
 Set Matrix Zeroes
 Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
 
-You must do it in place.*/
+You must do it in place.
+*/
 
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean firstRowZero = false;
-        boolean firstColZero = false;
+        boolean rowZ = false;
+        boolean colZ = false;
 
         for (int i = 0; i < matrix.length; i++) {
             if (matrix[i][0] == 0) {
-                firstColZero = true;
+                colZ = true;
                 break;
             }
         }
         for (int j = 0; j < matrix[0].length; j++) {
             if (matrix[0][j] == 0) {
-                firstRowZero = true;
+                rowZ = true;
                 break;
             }
         }
@@ -39,12 +40,12 @@ class Solution {
             }
         }
 
-        if (firstRowZero) {
+        if (rowZ) {
             for (int j = 0; j < matrix[0].length; j++) {
                 matrix[0][j] = 0;
             }
         }
-        if (firstColZero) {
+        if (colZ) {
             for (int i = 0; i < matrix.length; i++) {
                 matrix[i][0] = 0;
             }
