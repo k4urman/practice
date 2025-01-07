0c3b159a9636a19dce6c72a62ae06681bf096755
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-01-07 12:22:43 -0500

    Update2 dailyTwelve.java

--- dailyproblems/dailyTwelve.java
+++ dailyproblems/dailyTwelve.java
@@ -6,26 +6,24 @@ You must do it in place.*/
 
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
         boolean firstRowZero = false;
         boolean firstColZero = false;
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             if (matrix[i][0] == 0) {
                 firstColZero = true;
                 break;
             }
         }
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < matrix[0].length; j++) {
             if (matrix[0][j] == 0) {
                 firstRowZero = true;
                 break;
             }
         }
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
@@ -33,8 +31,8 @@ class Solution {
             }
         }
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
@@ -42,12 +40,12 @@ class Solution {
         }
 
         if (firstRowZero) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 matrix[0][j] = 0;
             }
         }
         if (firstColZero) {
-            for (int i = 0; i < m; i++) {
+            for (int i = 0; i < matrix.length; i++) {
                 matrix[i][0] = 0;
             }
         }
