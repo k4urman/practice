ecefbd7a97948814a60f07e5d033014bd5ba56f1
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-01-10 12:23:25 -0500

    Update5 dailyFourteen.java

--- dailyproblems/dailyFourteen.java
+++ dailyproblems/dailyFourteen.java
@@ -19,15 +19,14 @@ Explanation: Your function can return either index number 1 where the peak eleme
 
 class Solution {
     public int findPeakElement(int[] nums) {
-        int n = nums.length;
 
-        if (n == 1 || nums[0] > nums[1]) {
+        if (nums.length == 1 || nums.length[0] > nums.length[1]) {
             return 0;
         }
-        if (nums[n - 1] > nums[n - 2]) {
+        if (nums[nums.length - 1] > nums[nums.length - 2]) {
             return n - 1;
         }
-        for (int i = 1; i < n - 1; i++) {
+        for (int i = 1; i < nums.length - 1; i++) {
             if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
                 return i;
             }
