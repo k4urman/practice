799bf6391b6db79494af21398596c8c04a26be70
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-01-11 15:13:15 -0500

    Update1 dailyFifteen.java

--- dailyproblems/dailyFifteen.java
+++ dailyproblems/dailyFifteen.java
@@ -1,3 +1,11 @@
+/*
+Create Maximum Number
+
+You are given two integer arrays nums1 and nums2 of lengths m and n respectively. nums1 and nums2 represent the digits of two numbers. You are also given an integer k.
+Create the maximum number of length k <= m + n from digits of the two numbers. The relative order of the digits from the same array must be preserved.
+Return an array of the k digits representing the answer.
+*/
+
 class Solution {
     public int[] maxNumber(int[] nums1, int[] nums2, int k) {
         int[] total = new int[nums1.length + nums2.length];
