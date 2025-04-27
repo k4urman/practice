a0a2c2d6c074bde30757a10c37508a3b961bd08d
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-04-27 19:06:36 -0400

    Create code4_27_25.c

---
+++ problemv2/code4_27_25.c
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode* dummyHead = createNode(0);
+    struct ListNode* current = dummyHead;
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int sum = carry;
+
+        if (l1 != NULL) {
+            sum += l1->value;
+            l1 = l1->next;
+        }
+
+        if (l2 != NULL) {
+            sum += l2->value;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+        current->next = createNode(sum % 10);
+        current = current->next;
+    }
+
+    return dummyHead->next;
+}
