9fe97ce0a26928cb64c3e8e0852266a7f2891a12
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-05-10 10:47:25 -0400

    Update code5_10_25.c

--- problemv2/code5_10_25.c
+++ problemv2/code5_10_25.c
@@ -1,5 +1,32 @@
-#include <stdlib.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-int myAtoi(char* s) {
-    return atoi(s);
+int myAtoi(char *s) {
+  int i = 0;
+  int sign = 1;
+  long result = 0;
+
+  while (s[i] == ' ') {
+    i++;
+  }
+
+  if (s[i] == '+' || s[i] == '-') {
+    sign = (s[i] == '-') ? -1 : 1;
+    i++;
+  }
+
+  while (s[i] >= '0' && s[i] <= '9') {
+    result = result * 10 + (s[i] - '0');
+
+    if (result * sign > INT_MAX) {
+      return INT_MAX;
+    }
+    if (result * sign < INT_MIN) {
+      return INT_MIN;
+    }
+    i++;
+  }
+
+  return (int)(result * sign);
 }
