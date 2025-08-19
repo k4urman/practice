343530d2aaaa222c2f734ef6bc70b453d8d65539
Maninder Kaur <75804314+k4urman@users.noreply.github.com>
2025-08-19 12:37:54 -0400

    final powerof4.c

--- leetcode/random/powerof4.c
+++ leetcode/random/powerof4.c
@@ -25,7 +25,8 @@ Constraints:
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        return ((n & (n - 1)) == 0) &&
+        return (n > 0) &&
+               ((n & (n - 1)) == 0) &&
                ((n & 0x55555555) != 0);
     }
 };
