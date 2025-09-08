421f15bde7b4cc6003d5a239a6a657fb4033ae13
Maninder Kaur <75804314+k4urman@users.noreply.github.com>
2025-09-08 11:22:22 -0400

    Update lengthOfLastWord.c

--- leetcode/random/lengthOfLastWord.c
+++ leetcode/random/lengthOfLastWord.c
@@ -33,12 +33,13 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int count = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(s[i] != ' ' ){
+        for(int i = s.length()-1; i >= 0; i--){
+            if(s[i] != ' '){
                 count++;
-            } else if(s[i] == ' '){
-                count = 0;
-            } 
+                if(i == 0 || s[i-1] == ' '){
+                    break;
+                }
+            }
         }
         return count;
     }
