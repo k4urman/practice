0f24964362f262b7940a4ed862cd4c75f604141b
Maninder Kaur <75804314+k4urman@users.noreply.github.com>
2025-09-27 11:18:42 -0400

    2 climbingStairs.java

--- leetcode/random/climbingStairs.java
+++ leetcode/random/climbingStairs.java
@@ -27,9 +27,7 @@ Constraints:
 
 class Solution {
     public int climbStairs(int n) {
-        if (n == 2){
-            return 2;
-        } else if (n == 1){
+        if (n == 1 || n == 0){
             return 1;
         } else{
             return climbStairs(n-1) + climbStairs(n-2);
