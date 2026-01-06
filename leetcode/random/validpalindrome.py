ce4ee8eff8625c2de9faf1e9d0256f95167c0619
k4urman <maninder1094@icloud.com>
2026-01-06 18:22:36 -0500

    validpalindrome final

--- leetcode/random/validpalindrome.py
+++ leetcode/random/validpalindrome.py
@@ -1,3 +1,22 @@
+
+"""
+FINAL ATTEMPT - ACCEPTED
+"""
+import string
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        n = ''
+        for c in s:
+            if c.isalnum():
+                n += c.lower()
+        return n == n[::-1]
+        
+
 """ FIRST ATTEMPT
 import string
 
