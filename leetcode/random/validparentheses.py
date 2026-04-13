cb0679ac0d626d0d4bd25a4fc0cd79a5bd37a548
k4urman <maninder1094@icloud.com>
2026-04-13 13:48:14 -0400

    valid parentheses solve

--- leetcode/random/validparentheses.py
+++ leetcode/random/validparentheses.py
@@ -33,3 +33,17 @@ Constraints:
     1 <= s.length <= 104
     s consists of parentheses only '()[]{}'.
 """
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")":"(", "}":"{", "]":"["}
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or mapping[char] != stack.pop():
+                    return False
+        
+        return not stack
