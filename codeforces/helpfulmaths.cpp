9b4e82c33d0a219c51b301155685ace582d24330
k4urman <maninder1094@icloud.com>
2026-05-18 14:32:44 -0400

    horshoe but failed helpful maths

---
+++ codeforces/helpfulmaths.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s, ans;
+    int plus(0);
+
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '+') {
+            plus++;
+
+        }
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+    
+    }
+
+
+
+
+    
+
+
+
+
+
+
+    
+}
---
+++ codeforces/horseshoreontheotherhoof.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, count(0);
+
+    cin >> a >> b >> c >> d;
+
+    if (a == b || a == c || a == d) {
+        count++;
+    }
+
+    if (b == c || b == d) {
+        count++;
+    }
+
+    if (c == d) {
+        count++;
+    }
+
+    cout << count << endl;
+    return 0;
+}
