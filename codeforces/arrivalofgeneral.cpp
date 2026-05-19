d6f40e9d7c4a585a4ac3967597f29e80b2e5e53b
k4urman <maninder1094@icloud.com>
2026-05-19 13:29:56 -0400

    arrival of the general

---
+++ codeforces/arrivalofgeneral.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> a(n);
+    int max_val = 0, min_val = 101;
+    int max_idx = 0, min_idx = 0;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] > max_val) {
+            max_val = a[i];
+            max_idx = i;
+        }
+        if (a[i] <= min_val) {
+            min_val = a[i];
+            min_idx = i;
+        }
+    }
+    
+    int total_swaps = max_idx + (n - 1 - min_idx);
+    if (max_idx > min_idx) {
+        total_swaps--;
+    }
+    
+    cout << total_swaps << endl;
+    
+    return 0;
+}
+
