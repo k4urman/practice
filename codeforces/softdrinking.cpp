e60db72bf1188162b9c92a8a1758cdb303db6782
k4urman <maninder1094@icloud.com>
2026-05-19 21:57:18 -0400

    soft drinking desc

--- codeforces/softdrinking.cpp
+++ codeforces/softdrinking.cpp
@@ -1,3 +1,26 @@
+/*
+151A. Soft Drinking
+
+This winter is so cold in Nvodsk! A group of n friends decided to buy k bottles of a soft drink called "Take-It-Light" to warm up a bit. Each bottle has l milliliters of the drink. Also they bought c limes and cut each of them into d slices. After that they found p grams of salt.
+
+To make a toast, each friend needs nl milliliters of the drink, a slice of lime and np grams of salt. The friends want to make as many toasts as they can, provided they all drink the same amount. How many toasts can each friend make?
+Input
+
+The first and only line contains positive integers n, k, l, c, d, p, nl, np, not exceeding 1000 and no less than 1. The numbers are separated by exactly one space.
+Output
+
+Print a single integer — the number of toasts each friend can make.
+
+3 4 5 10 8 100 3 1
+2
+
+5 100 10 1 19 90 4 3
+3
+
+10 1000 1000 25 23 1 50 1
+0
+*/
+
 #include <iostream>
 #include <algorithm>
 
